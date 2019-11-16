8c24a750dce6e2c6fa0cfbc250f770f78e91b7f7
Я <dmitriyKa3antsev@yandex.ru>
2019-11-16 20:38:09 +0300

    ...

---
+++ PipeC#/Client/polygon2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace PipeApplication1
+{
+    class ProgramPipeTest
+    {
+        public static void ThreadStartClient(object obj)
+        {
+            using (NamedPipeClientStream pipeStream = new NamedPipeClientStream("mytestpipe"))
+            {
+                pipeStream.Connect();
+                FileStream file = new FileStream("/home/r3pl1c4nt/Projects/2.txt", FileMode.Open);
+                Console.WriteLine("[Client] Pipe connection established");
+                using (StreamWriter sw = new StreamWriter(pipeStream))
+                {
+                    sw.AutoFlush = true;
+                    StreamReader sr = new StreamReader(file);
+                    sw.WriteLine(sr.ReadLine());
+                    sr.Dispose();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ProgramPipeTest Client = new ProgramPipeTest();
+
+            Thread ClientThread = new Thread(Client.ThreadStartClient);
+            ClientThread.Start();
+        }
+    }
+}
---
+++ PipeC#/Server/polygon/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace PipeApplication1
+{
+    class ProgramPipeTest
+    {
+        public static void ThreadStartServer()
+        {
+            using (NamedPipeServerStream pipeStream = new NamedPipeServerStream("mytestpipe"))
+            {
+                Console.WriteLine("[Server] Pipe created {0}", pipeStream.GetHashCode());
+                pipeStream.WaitForConnection();
+                Console.WriteLine("[Server] Pipe connection established");
+                FileStream file = new FileStream("/home/r3pl1c4nt/Projects/1.txt", FileMode.Append);
+                
+                using (StreamReader sr = new StreamReader(pipeStream))
+                {
+                    StreamWriter sw = new StreamWriter(file);
+                    sw.WriteLine(sr.ReadLine());
+                    sw.Dispose();
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            ProgramPipeTest Server = new ProgramPipeTest();
+
+            Thread ServerThread = new Thread(Server.ThreadStartServer);
+
+            ServerThread.Start();
+
+        }
+    }
+}
